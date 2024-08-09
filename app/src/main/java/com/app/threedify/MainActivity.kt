049681fca8e5de5b9d2a32660fbe7f53c3d73dad
Richie_Vist <vistovschiiserghei@gmail.com>
2024-08-09 14:39:07 +0300

    Merge feature-iterface

--- app/src/main/java/com/app/threedify/MainActivity.kt
+++ app/src/main/java/com/app/threedify/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.app.threedify
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,7 +19,6 @@ import com.app.threedify.databinding.ActivityMainBinding
 import org.the3deer.app.model3D.view.ModelActivity
 import org.the3deer.util.android.AndroidURLStreamHandlerFactory
 import java.net.URL
-import com.app.threedify.databinding.ActivitySplashBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -31,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var switchTheme: Switch
-    private lateinit var navController: NavController
+//    private lateinit var navController: NavController
 
     /////////////////////////////////////////////////////////////////
     //Should exist in every class which uses activities from VIEWERAPP
@@ -53,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //setSupportActionBar(binding.appBarMain.toolbar)
+        setSupportActionBar(binding.appBarMain.toolbar)
         //navController = findNavController(R.id.nav_host_fragment_content_main)
 
         //open camera
@@ -62,47 +60,49 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-        //val drawerLayout: DrawerLayout = binding.drawerLayout
-        //val navView: NavigationView = binding.navView
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
 
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(
-////                R.id.nav_home, R.id.nav_gallery, R.id.nav_camera, R.id.nav_about_us, R.id.nav_settings
-//            ), drawerLayout
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_camera, R.id.nav_about_us, R.id.nav_settings
+            ), drawerLayout
+        )
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        //navView.setupWithNavController(navController)
 
 
         //theme night - light
-//        switchTheme = navView.findViewById(R.id.switch_theme)
-//        switchTheme.isChecked = getSavedThemeState()
-//
-//        switchTheme.setOnCheckedChangeListener { _, isChecked ->
-//            if (isChecked) {
-//                setDarkTheme()
-//            } else {
-//                setLightTheme()
-//            }
-//            saveThemeState(isChecked)
-//        }
-//        if (switchTheme.isChecked) {
-//            setDarkTheme()
-//        } else {
-//            setLightTheme()
-//        }
+        switchTheme = navView.findViewById(R.id.switch_theme)
+        switchTheme.isChecked = getSavedThemeState()
+
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                setDarkTheme()
+            } else {
+                setLightTheme()
+            }
+            saveThemeState(isChecked)
+        }
+        if (switchTheme.isChecked) {
+            setDarkTheme()
+        } else {
+            setLightTheme()
+        }
 // //////////////////////////////////////////////////
         ///////////////////////////////////////////////
         //////////////////////////////////////////////////
         //////////////////////////////////////////////////
 
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
 
         /////////////////////////////////////////////////////////////////
@@ -124,6 +124,30 @@ class MainActivity : AppCompatActivity() {
     //////////////////
     ////for interface
     //////////////////
+
+//    private fun openCameraFragment() {
+//        navController.navigate(R.id.nav_camera)
+//    }
+    private fun setLightTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun setDarkTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+
+    private fun getSavedThemeState(): Boolean {
+        val sharedPreferences = getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("isDarkTheme", false)
+    }
+
+    private fun saveThemeState(isDarkTheme: Boolean) {
+        val sharedPreferences = getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isDarkTheme", isDarkTheme)
+        editor.apply()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         //menuInflater.inflate(R.menu.main, menu)
 
@@ -132,8 +156,8 @@ class MainActivity : AppCompatActivity() {
     //////////////////
     ////for interface
     //////////////////
-    override fun onSupportNavigateUp(): Boolean {
-        //val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        //val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
 }
