a129d42c158bfaf3b660827c061cd3b0e97bb8d0
MaryLens <marycravts@gmail.com>
2024-08-09 13:20:54 +0300

    main menu added

--- app/src/main/java/com/app/threedify/MainActivity.kt
+++ app/src/main/java/com/app/threedify/MainActivity.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 import org.the3deer.app.model3D.view.ModelActivity
 import org.the3deer.util.android.AndroidURLStreamHandlerFactory
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
 
     /////////////////////////////////////////////////////////////////
     //Should exist in every class which uses activities from VIEWERAPP
@@ -26,12 +25,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+/*        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
 
 
         /////////////////////////////////////////////////////////////////
