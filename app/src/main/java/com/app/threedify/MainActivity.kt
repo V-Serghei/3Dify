9ff91ef36b06ea26a32454e552c085c11c6938d1
Richie_Vist <vistovschiiserghei@gmail.com>
2024-08-09 13:23:43 +0300

    MainActivity setting

--- app/src/main/java/com/app/threedify/MainActivity.kt
+++ app/src/main/java/com/app/threedify/MainActivity.kt
@@ -2,16 +2,36 @@ package com.app.threedify
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.app.threedify.databinding.ActivityMainBinding
 
 import org.the3deer.app.model3D.view.ModelActivity
 import org.the3deer.util.android.AndroidURLStreamHandlerFactory
 import java.net.URL
+import com.app.threedify.databinding.ActivitySplashBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    //////////////////
+    ////for interface
+    //////////////////
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var switchTheme: Switch
+    private lateinit var navController: NavController
 
     /////////////////////////////////////////////////////////////////
     //Should exist in every class which uses activities from VIEWERAPP
@@ -25,6 +45,57 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        //////////////////
+        ////for interface
+        //////////////////
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setSupportActionBar(binding.appBarMain.toolbar)
+        //navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        //open camera
+//        binding.appBarMain.fab.setOnClickListener {
+//            openCameraFragment()
+//        }
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        //val drawerLayout: DrawerLayout = binding.drawerLayout
+        //val navView: NavigationView = binding.navView
+
+//        appBarConfiguration = AppBarConfiguration(
+//            setOf(
+////                R.id.nav_home, R.id.nav_gallery, R.id.nav_camera, R.id.nav_about_us, R.id.nav_settings
+//            ), drawerLayout
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+
+
+        //theme night - light
+//        switchTheme = navView.findViewById(R.id.switch_theme)
+//        switchTheme.isChecked = getSavedThemeState()
+//
+//        switchTheme.setOnCheckedChangeListener { _, isChecked ->
+//            if (isChecked) {
+//                setDarkTheme()
+//            } else {
+//                setLightTheme()
+//            }
+//            saveThemeState(isChecked)
+//        }
+//        if (switchTheme.isChecked) {
+//            setDarkTheme()
+//        } else {
+//            setLightTheme()
+//        }
+// //////////////////////////////////////////////////
+        ///////////////////////////////////////////////
+        //////////////////////////////////////////////////
+        //////////////////////////////////////////////////
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -46,5 +117,23 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("backgroundColor", "1.0 1.0 1.0 1.0")
         startActivity(intent)
         /////////////////////////////////////////////////////////////////
+    }
+
+
+
+    //////////////////
+    ////for interface
+    //////////////////
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //menuInflater.inflate(R.menu.main, menu)
+
+        return true
+    }
+    //////////////////
+    ////for interface
+    //////////////////
+    override fun onSupportNavigateUp(): Boolean {
+        //val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
