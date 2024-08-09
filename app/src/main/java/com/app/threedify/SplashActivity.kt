497ff36eec1bcfad3c9bd7bec3f7b56f13102409
Richie_Vist <vistovschiiserghei@gmail.com>
2024-08-09 12:05:20 +0300

    Loading animation added

---
+++ app/src/main/java/com/app/threedify/SplashActivity.kt
@@ -0,0 +1,52 @@
+package com.app.threedify
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashAnimation: AnimationDrawable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        applySavedTheme()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splashImage = findViewById<ImageView>(R.id.splashImage)
+        val sharedPreferences = getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE)
+        val isDarkTheme = sharedPreferences.getBoolean("isDarkTheme", false)
+
+        if (isDarkTheme) {
+            splashImage.setBackgroundResource(R.drawable.animation)
+        } else {
+            splashImage.setBackgroundResource(R.drawable.animation_white_them)
+        }
+
+        splashAnimation = splashImage.background as AnimationDrawable
+
+        splashImage.post { splashAnimation.start() }
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+
+    private fun applySavedTheme() {
+        val sharedPreferences = getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE)
+        val isDarkTheme = sharedPreferences.getBoolean("isDarkTheme", false)
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+}
