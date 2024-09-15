77639a5440bc39824b754b323920b49334c4b2e3
Richie_Vist <vistovschiiserghei@gmail.com>
2024-09-15 14:17:58 +0300

    merge the "feature-building3DObj" branch with the "develop" branch

--- app/src/main/java/com/app/threedify/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/app/threedify/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,6 @@
 package com.app.threedify.ui.gallery
 
-import android.content.ContentValues
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.app.threedify.databinding.FragmentGalleryBinding
-import com.example.nativelib.Model3DCreator
-import java.io.OutputStream
 
 class GalleryFragment : Fragment() {
 
@@ -34,54 +29,13 @@ class GalleryFragment : Fragment() {
             textView.text = it
         }
 
-        val model3DCreator = Model3DCreator()
-        val pointArrays = arrayOf(
-            floatArrayOf(0f, 0f, 0f, 1f, 0f, 0f, 1f, 1f, 0f, 0f, 1f, 0f),
-            floatArrayOf(0f, 0f, 0f, 1f, 0f, 0f, 0.5f, 1f, 1f),
-            floatArrayOf(1f, 0f, 0f, 1f, 1f, 0f, 0.5f, 1f, 1f),
-            floatArrayOf(1f, 1f, 0f, 0f, 1f, 0f, 0.5f, 1f, 1f),
-            floatArrayOf(0f, 0f, 0f, 0f, 1f, 0f, 0.5f, 1f, 1f)
-        )
-
-
         try {
-            textView.text = "1111111111111111111111111111111111111" +
-                    "1111111111111111111111111111" +
-                    "1111111111111111111" +
-                    "11" +
-                    "1" +
-                    "1" +
-                    "1" +
-                    "1" +
-                    "1" +
-                    "1" +
-                    "" +
-                    "1Point Cloud Processing and Saving: Success"
+            textView.text = "Point Cloud Processing and Saving: Success"
         } catch (e: Exception) {
             textView.append("\n22222222222222222222222222222222222222Point Cloud Processing: Failed\n${e.message}")
         }
 
         return root
-    }
-
-    private fun saveModelToFile(objData: String, filename: String) {
-        val resolver = requireContext().contentResolver
-        val values = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, "$filename.obj")
-            put(MediaStore.MediaColumns.MIME_TYPE, "application/octet-stream")
-            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
-        }
-
-        val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values)
-        uri?.let {
-            resolver.openOutputStream(it)?.use { outputStream ->
-                writeDataToFile(outputStream, objData)
-            }
-        }
-    }
-
-    private fun writeDataToFile(outputStream: OutputStream, data: String) {
-        outputStream.write(data.toByteArray())
     }
 
     override fun onDestroyView() {
